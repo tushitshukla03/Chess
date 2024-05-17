5e73c20ff9ce04c4fa5cb850c44372d3b5732b45
tushit03 <tushit.shukla03@gmail.com>
2024-05-17 16:38:40 +0530

    Chess

---
+++ Chess/ChessMain.py
@@ -0,0 +1,14 @@
+import pygame as p
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+
+def loadImages():
+    pieces = ['wP', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bP', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load('images/' + piece + '.png'), (SQ_SIZE, SQ_SIZE))
