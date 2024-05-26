44adabed04655fe675b228d0e1e19fd777fec8ae
Tushit Shukla <97801384+tushitshukla03@users.noreply.github.com>
2024-05-27 00:01:13 +0530

    Update ChessEngine.py

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -1,6 +1,6 @@
 class GameState():
     def __init__(self):
-        # Board is an 8x8 2d list, each element of the list has 2 characters.
+        # Board    is an 8x8 2d list, each element of the list has 2 characters.
         # The first character represents the color of the piece, 'b' or 'w'
         # The second character represents the type of the piece, 'K', 'Q', 'R', 'B', 'N', 'P'
         # "--" represents an empty space with no piece.
